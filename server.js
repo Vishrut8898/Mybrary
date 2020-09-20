480806fbd3370b5a52fa9b5eae91f4c97d80fb15
Vishrut8898 <vishrut9869@gmail.com>
2020-09-20 17:04:22 +0530

    49 min video done

--- server.js
+++ server.js
@@ -1,38 +1,31 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
 }
 
-const express = require("express");
-const app = express();
-const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
-const indexRouter = require("./routes/index");
-const authorRouter = require("./routes/authors");
-const bookRouter = require("./routes/books");
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-app.set("layout", "layouts/layout");
-app.use(expressLayouts);
-app.use(express.static("/public"));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-// Mongoose
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to MongoDB."));
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
-// Routes
-app.use("/", indexRouter);
-app.use("/authors", authorRouter);
-app.use("/books", bookRouter);
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Ready To Go!");
-});
+app.listen(process.env.PORT || 3000)
